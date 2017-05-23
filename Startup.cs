2a26d94cc0727bcec7a719708b43b0a2766bc8a0
Optofizik <optofizik@gmail.com>
2017-05-23 22:55:29 +0300

    first commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SpendingsCalculator.Startup))]
+namespace SpendingsCalculator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
