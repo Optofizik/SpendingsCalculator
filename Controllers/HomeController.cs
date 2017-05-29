28db2fb8bfa22cb2b0b49b09ba60501508bae3e1
Optofizik <optofizik@gmail.com>
2017-05-30 00:40:33 +0300

    Third commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,24 +10,7 @@ namespace SpendingsCalculator.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
-            ApplicationContext context = new ApplicationContext();
-            context.Tag.Add(new Tag() { Id = 1, Name = "First" });
-            context.SaveChanges();
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+        {            
             return View();
         }
     }
